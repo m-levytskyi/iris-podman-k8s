d6388dbd0b501bc478ffbc9b949c942dd4eef1cc
Mykhailo Levytskyi <qwertymaden@gmail.com>
2025-02-17 10:46:52 +0100

    add predicted class names

--- predict.py
+++ predict.py
@@ -4,6 +4,9 @@ import joblib
 app = Flask(__name__)
 model = joblib.load('/app/model.pkl')
 
+# Map class indices to class names
+class_names = {0: "setosa", 1: "versicolor", 2: "virginica"}
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -17,7 +20,8 @@ def predict():
         float(request.form['petal_width'])
     ]
     prediction = model.predict([features])
-    return render_template('result.html', prediction=prediction[0])
+    predicted_class_name = class_names[prediction[0]]
+    return render_template('result.html', prediction=predicted_class_name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
