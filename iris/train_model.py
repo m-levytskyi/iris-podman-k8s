e88dd18febfc3ba871c3595f7b42697b80f74e70
Mykhailo Levytskyi <qwertymaden@gmail.com>
2025-03-28 12:24:07 +0100

    refactoring

--- iris/train_model.py
+++ iris/train_model.py
@@ -4,20 +4,30 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
-def load_data(db_path):
-    conn = sqlite3.connect(db_path)
+def load_data(database_path):
+    conn = sqlite3.connect(database_path)
     query = "SELECT * FROM iris"
     df = pd.read_sql_query(query, conn)
     conn.close()
     return df
 
 def train_model(df):
+    """
+    Trains a RandomForestClassifier model on the given DataFrame.
+
+    Parameters:
+    df (pandas.DataFrame): The input DataFrame containing the features and target variable. 
+                           It is expected to have a column named 'species' which is the target variable.
+
+    Returns:
+    RandomForestClassifier: The trained RandomForestClassifier model.
+    """
     X = df.drop(columns=['species'])
     y = df['species']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model = RandomForestClassifier()
-    model.fit(X_train, y_train)
-    return model
+    X_train, _, y_train, _ = train_test_split(X, y, test_size=0.2, random_state=42)
+    rf_model = RandomForestClassifier()
+    rf_model.fit(X_train, y_train)
+    return rf_model
 
 if __name__ == "__main__":
     db_path = '/app/iris.db'
